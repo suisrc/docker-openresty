f78a30a1f5fcc3fdfa12bf8149923b5de64b6962
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2022-08-18 15:15:35 +0800

    update hp

---
+++ auto-reload1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+nginx -g "daemon off;" &
+inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T' /etc/nginx/conf.d/ | while read date time; do
+    echo "At ${time} on ${date}, config file update detected."
+    nginx -s reload
+done
+
+## /usr/local/openresty/nginx/sbin/nginx
