d97cc8a90e5d99302cc94afa7579b2428264603f
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2022-08-18 11:17:02 +0800

    rm other

--- auto-reload-0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/usr/bin/openresty -g "daemon off;" &
-inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T' ${NGINX_CONF} | while read date time; do
-    echo "At ${time} on ${date}, config file update detected."
-    nginx -s reload
-done
