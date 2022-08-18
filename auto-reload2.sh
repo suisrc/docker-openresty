bf4ab11ac18de52c9da8eda548e9c4f83824626e
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2022-08-18 16:54:42 +0800

    update az

--- auto-reload2.sh
+++ auto-reload2.sh
@@ -4,16 +4,16 @@ if [ $KS_WATCHDOG ]; then ## 看门狗模式
     while read line; do  vars=$vars"\${${line%%;*}} "; done < /etc/nginx/kg/env.conf
     envsubst  "$vars" < /etc/nginx/kg/nginx.conf   > /usr/local/openresty/nginx/conf/nginx.conf
 
-    if [[ $KS_WATCHDOG =~ 'authx' ]] then ## 登录鉴权
+    if [[ $KS_WATCHDOG =~ 'authx' ]]; then ## 登录鉴权
         envsubst  "$vars" < /etc/nginx/kg/authx.conf > /etc/nginx/conf.d/authx.conf
     fi
-    if [[ $KS_WATCHDOG =~ 'authz' ]] then ## 接口鉴权
+    if [[ $KS_WATCHDOG =~ 'authz' ]]; then ## 接口鉴权
         envsubst  "$vars" < /etc/nginx/kg/authz.conf > /etc/nginx/conf.d/authz.conf
     fi
-    if [[ $KS_WATCHDOG =~ 'proxyp' ]] then ## path_proxy代理
+    if [[ $KS_WATCHDOG =~ 'proxyp' ]]; then ## path_proxy代理
         envsubst  "$vars" < /etc/nginx/kg/proxyp.conf > /etc/nginx/conf.d/proxyp.conf
     fi
-    if [[ $KS_WATCHDOG =~ 'proxyh' ]] then ## http_proxy代理
+    if [[ $KS_WATCHDOG =~ 'proxyh' ]]; then ## http_proxy代理
         envsubst  "$vars" < /etc/nginx/kg/proxyh.conf > /etc/nginx/conf.d/proxyh.conf
     fi
 fi
