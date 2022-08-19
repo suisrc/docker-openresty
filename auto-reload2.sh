c9894fb7f63fd772b3bdc1640e53b34d9ebce80e
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2022-08-19 18:47:32 +0800

    增加出站数据监控方式

--- auto-reload2.sh
+++ auto-reload2.sh
@@ -13,13 +13,22 @@ if [ $KS_WATCHDOG ]; then ## 看门狗模式
         envsubst  "$vars" < /etc/nginx/kg/authz.conf > /etc/nginx/conf.d/authz.conf
         echo "envsubst /etc/nginx/kg/authz.conf"
     fi
-    if [[ $KS_WATCHDOG =~ 'proxyp' ]]; then ## path_proxy代理
-        envsubst  "$vars" < /etc/nginx/kg/proxyp.conf > /etc/nginx/conf.d/proxyp.conf
-        echo "envsubst /etc/nginx/kg/proxyp.conf"
+    if [[ $KS_WATCHDOG =~ 'pxy_p' ]]; then ## path_proxy代理
+        envsubst  "$vars" < /etc/nginx/kg/pxy_p.conf > /etc/nginx/conf.d/pxy_p.conf
+        echo "envsubst /etc/nginx/kg/pxy_p.conf"
     fi
-    if [[ $KS_WATCHDOG =~ 'proxyh' ]]; then ## http_proxy代理
-        envsubst  "$vars" < /etc/nginx/kg/proxyh.conf > /etc/nginx/conf.d/proxyh.conf
-        echo "envsubst /etc/nginx/kg/proxyh.conf"
+    if [[ $KS_WATCHDOG =~ 'pxy_h' ]]; then ## http_proxy代理
+        envsubst  "$vars" < /etc/nginx/kg/pxy_h.conf > /etc/nginx/conf.d/pxy_h.conf
+        echo "envsubst /etc/nginx/kg/pxy_h.conf"
+    fi
+    if [[ $KS_WATCHDOG =~ 'pxy_a' ]]; then ## all_proxy代理
+        envsubst  "$vars" < /etc/nginx/kg/pxy_a.conf > /etc/nginx/conf.d/pxy_a.conf
+        echo "envsubst /etc/nginx/kg/pxy_a.conf"
+    fi
+    if [[ $KS_WATCHDOG =~ 'pxy_i' ]]; then ## iptables_proxy代理
+        envsubst  "$vars" < /etc/nginx/kg/pxy_i.conf   > /etc/nginx/conf.d/pxy_i.conf
+        envsubst  "$vars" < /etc/nginx/kg/pxy_i.stream > /etc/nginx/conf.d/pxy_i.stream
+        echo "envsubst /etc/nginx/kg/pxy_i.conf"
     fi
 fi
 
