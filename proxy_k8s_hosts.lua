9ea7d901cab327d51ab3f762a420967169852d26
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2022-08-22 20:20:55 +0800

    fix clientAddr

--- proxy_k8s_hosts.lua
+++ proxy_k8s_hosts.lua
@@ -1,4 +1,23 @@
 -- 修正，代理服务器域名解析问题
+
+-- 通过/etc/hosts文件获取局域网IP
+if loc_area_ip == nil then
+    local f_host = io.open("/etc/hosts")
+    if f_host then -- 获取本地局域网IP
+        for line in f_host:lines() do
+            local ip, host = string.match(line, "^(%d+%.%d+%.%d+%.%d+)%s+([%w-_%.]*)")
+            if host ~= nil and string.find(host, "localhost") == nil then 
+                loc_area_ip = ip
+                loc_area_name = host
+                break  
+            end
+        end
+        f_host:close()
+    end
+    if loc_area_ip == nil then  loc_area_ip = "" end
+    ngx.log(ngx.ERR, "loc_area_ip:"..loc_area_ip..", loc_area_name:"..loc_area_name..";")
+end
+
 if lan_domain == nil then 
     lan_domain = "."..os.getenv("LUA_PROXY_LAN_M") -- 读取环境变量
     ngx.log(ngx.ERR, "lan_domain:", lan_domain)
