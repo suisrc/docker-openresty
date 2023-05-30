77032dce629130360bf796cf18eb14abdcaa5784
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2023-05-30 15:17:57 +0800

    fix time nginx

--- auto-reload1.sh
+++ auto-reload1.sh
@@ -5,7 +5,7 @@ nginx -g "daemon off;" &
 
 # 监控nginx配置文件变化，自动reload
 last_time=
-inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T' /etc/nginx/conf.d/ | while read date time; do
+inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M:%S.%N' --format '%T' /etc/nginx/conf.d/ | while read date time; do
     # 如果最后修改的时间和当前时间相同，不执行
     if [[ "$last_time" == "$time" ]]; then
         continue
